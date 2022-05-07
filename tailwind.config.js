5eb508305794a921159ed9cb33de28d77f37ad1a
tritranduc <tritranduc11@gmail.com>
2022-05-07 10:54:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./**/*.html", "./**/*.{vue,js,ts,jsx,tsx}"],
+  mode: "jit",
+  purge: ["./**/*.html"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
